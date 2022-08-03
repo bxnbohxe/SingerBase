7bfec270ce7f59f505b04e1ea542546cd9166ff7
VolTorian <voltorian.social@gmail.com>
2022-08-02 17:06:59 -0700

    updated driver
    adding more options in the driver for the user to choose

--- SingerBase/Driver.java
+++ SingerBase/Driver.java
@@ -24,31 +24,38 @@ public class Driver
 			
 			String selection;
 			Scanner scan = new Scanner(System.in);//currently set as if the user will interact through terminal, will need to change
-			System.out.println("what would you like to do?");
+			System.out.println("What would you like to do?");
+			System.out.println("1) create account\n2) login\n3) exit");
 			selection = scan.nextLine();
 			
 			while (!selection.equals("exit")) {
-				switch (selection) {
-				case "create account":
+				switch (selection.toUpperCase()) {
+				case "1":
+				case "CREATE ACCOUNT":
 					System.out.println("creating account");
 					if (proxy.createAccount()) {
 						
 					}
 					break;
-				case "login":
+				case "2":
+				case "LOGIN":
 					System.out.println("logging in");
 					if (proxy.login()) {
+						
 						loggedIn(proxy);
 					}
 					break;
-				case "exit":
+				case "3":
+				case "EXIT":
 					System.out.println("exiting");
+					scan.close();
 					C.close();
 					break;
-				case "test":
+				case "TEST":
 					proxy.testQuery();
 					break;
 				default:
+					System.out.println("Invalid selection");
 					break;
 				}
 				
@@ -64,12 +71,26 @@ public class Driver
 	}
 	
 	static void loggedIn(SQLProxy proxy) {
-		try {
-			proxy.addToFavorites();
-		}
-		catch (Exception E) {
-			System.err.println(E);
-			E.printStackTrace();
-		}
+		System.out.println("Select an option:\n1) add song to favorites\n2) view favorites");
+		Scanner scan = new Scanner(System.in);
+		String option = scan.nextLine();
+		switch (option.toUpperCase()) {
+		case "1":
+		case "ADD SONG TO FAVORITES":
+			try {
+				proxy.addToFavorites();
+			}
+			catch (Exception E) {
+				System.err.println(E);
+				E.printStackTrace();
+			}
+			break;
+		case "2":
+		case "view favorites":
+			break;
+		default:
+			System.out.println("Invalid selection");
+			break;
+		}	
 	}
 }
