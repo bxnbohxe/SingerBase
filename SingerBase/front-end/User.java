e6a0bd67a47300daaf0e025c802e3aa47c989c9f
alextrnphn <107496475+alextrnphn@users.noreply.github.com>
2022-08-05 21:31:10 -0700

    Update User.java

--- SingerBase/front-end/User.java
+++ SingerBase/front-end/User.java
@@ -1,7 +1,19 @@
+package singerbase;
+
 public class User {
     public String name;
     public String email;
     public String phone;
-    public String address;
+    public String user_id;
     public String password;
+    
+    User() {
+        name = null;
+        email = null;
+        phone = null;
+        user_id = null;
+        password = null;
+    }
 }
+
+    
