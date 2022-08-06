dcc996b26845f03a2067edfcc36e073175c8c909
alextrnphn <107496475+alextrnphn@users.noreply.github.com>
2022-08-05 21:28:59 -0700

    Update DashboardForm.java
    fixing, and add on button and function

--- SingerBase/front-end/DashboardForm.java
+++ SingerBase/front-end/DashboardForm.java
@@ -2,18 +2,15 @@ package singerbase;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DashboardForm extends JFrame {
     JPanel dashboardPanel;
     JLabel lblAdmin;
-    JButton btnLogin, btnRegister, btnSearchSinger;
-
+    JButton btnLogin, btnRegister, btnSearchSinger, btnLogOut, btnSongs, btnExit;
+    User curUser = new User();
+    MySQLDB objMySQLDB = new MySQLDB();
+    
     public DashboardForm() {
         setTitle("Dashborad");
 //        setModal(true);
@@ -25,12 +22,18 @@ public class DashboardForm extends JFrame {
         lblAdmin = new JLabel();
         btnLogin = new JButton("LOGIN");
         btnSearchSinger = new JButton("SEARCH SINGER");
+        btnSongs = new JButton("MANAGE SONGS");
         btnRegister = new JButton("REGISTER");
-        dashboardPanel = new JPanel(new GridLayout(4, 1));
+        btnLogOut = new JButton("LOG-OUT");
+        btnExit = new JButton("EXIT");
+        dashboardPanel = new JPanel(new GridLayout(7, 1));
         dashboardPanel.add(lblAdmin);
         dashboardPanel.add(btnLogin);
         dashboardPanel.add(btnSearchSinger);
+        dashboardPanel.add(btnSongs);
         dashboardPanel.add(btnRegister); 
+        dashboardPanel.add(btnLogOut); 
+        dashboardPanel.add(btnExit); 
         add(dashboardPanel, BorderLayout.CENTER);
         
         boolean hasRegistredUsers = connectToDatabase();
@@ -39,12 +42,14 @@ public class DashboardForm extends JFrame {
             LoginForm loginForm = new LoginForm(DashboardForm.this);
             User user = loginForm.user;
 
-            if (user != null) {
-                lblAdmin.setText("User: " + user.name);
+            if (user.user_id != null) {
+                curUser = user;
+                lblAdmin.setText("User: " + curUser.name);
                 setLocationRelativeTo(null);
                 setVisible(true);
             }
             else {
+                lblAdmin.setText("Please Login...!");
                 dispose();
             }
         }
@@ -53,21 +58,23 @@ public class DashboardForm extends JFrame {
             RegistrationForm registrationForm = new RegistrationForm(this);
             User user = registrationForm.user;
 
-            if (user != null) {
-                lblAdmin.setText("User: " + user.name);
+            if (user.user_id != null) {
+                curUser = user;
+                lblAdmin.setText("User: " + curUser.name);
                 setLocationRelativeTo(null);
                 setVisible(true);
             }
             else {
+                lblAdmin.setText("Please Register...!");
                 dispose();
             }
         }
-        
+        btnExit.addActionListener(e -> { System.exit(0); });
         btnRegister.addActionListener(e -> { 
                 RegistrationForm registrationForm = new RegistrationForm(DashboardForm.this);
                 User user = registrationForm.user;
 
-                if (user != null) {
+                if (user.user_id != null) {
                     JOptionPane.showMessageDialog(DashboardForm.this,
                             "New user: " + user.name,
                             "Successful Registration",
@@ -75,51 +82,70 @@ public class DashboardForm extends JFrame {
                 }
             }
         );
+        btnLogOut.addActionListener(e -> { 
+            if(curUser.user_id != null) { 
+                curUser = new User();
+                lblAdmin.setText("Please Login...!");
+                JOptionPane.showMessageDialog(DashboardForm.this,
+                            "LogOut Successfull...!",
+                            "Message",
+                            JOptionPane.INFORMATION_MESSAGE);
+            }
+                else {
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "Login First...!","Message",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                
+            }
+        );
         btnLogin.addActionListener(e -> { 
+            if(curUser.user_id == null) { 
                 LoginForm loginForm = new LoginForm(DashboardForm.this);
             User user = loginForm.user;
 
-            if (user != null) {
-                lblAdmin.setText("User: " + user.name);
+            if (user.user_id != null) {
+                curUser = user;
+                lblAdmin.setText("User: " + curUser.name);
                 setLocationRelativeTo(null);
                 setVisible(true);
+            } else { lblAdmin.setText("Please Login...!"); }
             }
+                else {
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "You Are Already Logged In...!","Message",
+                            JOptionPane.ERROR_MESSAGE);
+                }
             }
         );
-       btnSearchSinger.addActionListener(e -> {
-
-            if(!lblAdmin.getText().equals("User: ")) { 
-                SearchSinger objSearchSinger = new SearchSinger(DashboardForm.this); }
-
-            else {
-                JOptionPane.showMessageDialog(DashboardForm.this,"User Must Login To Search Singer","Login Failed",JOptionPane.INFORMATION_MESSAGE);
+        btnSearchSinger.addActionListener(e -> { 
+                if(curUser.user_id != null) { SearchSinger objSearchSinger = new SearchSinger(DashboardForm.this, curUser); }
+                else {
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "User Must Login To Search Singer","Login Failed",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
             }
-   
-
-}
-
-);
+        );
+        btnSongs.addActionListener(e -> { 
+                if(curUser.user_id != null) { ManageSongs objManageSongs = new ManageSongs(DashboardForm.this, curUser); }
+                else {
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "User Must Login To Manage Songs","Login Failed",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        );
     }
 
     private boolean connectToDatabase() {
         boolean hasRegistredUsers = false;
 
-        final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
-        final String DB_URL = "jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
-        final String USERNAME = "root";
-        final String PASSWORD = "";
-
         try{
             //First, connect to MYSQL server and create the database if not created
-            Connection conn = DriverManager.getConnection(MYSQL_SERVER_URL, USERNAME, PASSWORD);
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS MyStore");
-            statement.close();
-            conn.close();
+            objMySQLDB.ExecuteNonQueryAtRoot("CREATE DATABASE IF NOT EXISTS MyStore");
 
-            //Second, connect to the database and create the table "users" if not created
-            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            statement = conn.createStatement();
+            //Second, connect to the database and create the table "users" if cot created
             String sql = "CREATE TABLE IF NOT EXISTS SB_User ("
                     + "user_id VARCHAR(5) NOT NULL,"
                     + "username VARCHAR(20) NOT NULL,"
@@ -130,11 +156,9 @@ public class DashboardForm extends JFrame {
                     + "country VARCHAR(30),"
                     + "PRIMARY KEY(user_id),"
                     + "FOREIGN KEY (country) REFERENCES Country (country_name))";
-            statement.executeUpdate(sql);
-
+            objMySQLDB.ExecuteNonQuery(sql);
             //check if we have users in the table users
-            statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM SB_User");
+            ResultSet resultSet = objMySQLDB.ExecuteDataAdapter("SELECT COUNT(*) FROM SB_User");
 
             if (resultSet.next()) {
                 int numUsers = resultSet.getInt(1);
@@ -142,14 +166,7 @@ public class DashboardForm extends JFrame {
                     hasRegistredUsers = true;
                 }
             }
-
-            statement.close();
-            conn.close();
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
+        }catch(Exception e){ e.printStackTrace(); }
         return hasRegistredUsers;
     }
 
